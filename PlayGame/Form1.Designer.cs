c9520f34cd3d76e735e83111ae26e4dbcf1729a3
temp <temp>
2016-09-16 13:03:19 -0400

    Add project files.

---
+++ PlayGame/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace PlayGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ClickToPlay = new System.Windows.Forms.Label();
+            this.GameWillStart = new System.Windows.Forms.Label();
+            this.Go = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ClickToPlay
+            // 
+            this.ClickToPlay.AutoSize = true;
+            this.ClickToPlay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ClickToPlay.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClickToPlay.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClickToPlay.Location = new System.Drawing.Point(37, 50);
+            this.ClickToPlay.Name = "ClickToPlay";
+            this.ClickToPlay.Size = new System.Drawing.Size(246, 71);
+            this.ClickToPlay.TabIndex = 0;
+            this.ClickToPlay.Text = "\r\nCLICK TO PLAY GAME\r\n\r\n";
+            this.ClickToPlay.Click += new System.EventHandler(this.ClickToPlay_Click);
+            // 
+            // GameWillStart
+            // 
+            this.GameWillStart.AutoSize = true;
+            this.GameWillStart.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GameWillStart.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.GameWillStart.Location = new System.Drawing.Point(23, 186);
+            this.GameWillStart.Name = "GameWillStart";
+            this.GameWillStart.Size = new System.Drawing.Size(0, 21);
+            this.GameWillStart.TabIndex = 1;
+            this.GameWillStart.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Go
+            // 
+            this.Go.AutoSize = true;
+            this.Go.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Go.Location = new System.Drawing.Point(137, 186);
+            this.Go.Name = "Go";
+            this.Go.Size = new System.Drawing.Size(0, 21);
+            this.Go.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(318, 370);
+            this.Controls.Add(this.Go);
+            this.Controls.Add(this.GameWillStart);
+            this.Controls.Add(this.ClickToPlay);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Arcade Central";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ClickToPlay;
+        private System.Windows.Forms.Label GameWillStart;
+        private System.Windows.Forms.Label Go;
+    }
+}
+
---
+++ PlayGame/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace PlayGame
+{
+    public partial class Form1 : Form
+    { 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ClickToPlay_Click(object sender, EventArgs e)
+        {
+            SoundPlayer beep1 = new SoundPlayer(Properties.Resources.beep_01a); //sets sound files to be used
+            SoundPlayer beep2 = new SoundPlayer(Properties.Resources.LastBeep);
+            Graphics formGraphics = this.CreateGraphics();
+            Graphics fg = this.CreateGraphics();
+            Font CFont = new Font("Lucida Console", 80, FontStyle.Bold);
+            Font RamFont = new Font("Lucida Console", 40, FontStyle.Bold);
+            SolidBrush Redbrush = new SolidBrush(Color.Red);
+            SolidBrush Whitebrush = new SolidBrush(Color.White);
+            Font drawFont = new Font("Arial", 6, FontStyle.Regular);
+            SolidBrush drawBrush = new SolidBrush(Color.Gray);
+
+            ClickToPlay.Text = " "; //changes text in click to play bex to nothing
+            ClickToPlay.BorderStyle = BorderStyle.None; //changes the border to not appear
+
+            Refresh();
+            Thread.Sleep(500); //put thread to spleep
+            GameWillStart.Text = "The Game Will Start In 3"; //display new line of text on the screen
+            beep2.Play(); //play beep2 file as set earlier
+
+            Refresh();
+            Thread.Sleep(1000);
+            GameWillStart.Text = "The Game Will Start In 2";
+            beep2.Play();
+
+            Refresh();
+            Thread.Sleep(1000);
+            GameWillStart.Text = "The Game Will Start In 1";
+            beep2.Play();
+            
+
+            Refresh();
+            Thread.Sleep(1000);
+            BackColor = Color.Green; //changes the backgrond colour from black to green
+            Go.BackColor = Color.Green; //change the text box color to green to prevent black box from appearing on screen
+            GameWillStart.Text = ""; //gets rid of text from gamewillstart text box
+            GameWillStart.BackColor = Color.Green;
+            beep1.Play(); //play beep1 file as set earlier
+            formGraphics.FillEllipse(Redbrush, 65, 125, 200, 100); //create red ellipse
+            fg.DrawString("C", CFont, Whitebrush, 70, 125); //create white 'c' on ellipse
+            fg.DrawString("rams", RamFont, Whitebrush, 115, 150); //create white 'rams' on ellipse
+            fg.TranslateTransform(275, 140);
+            fg.RotateTransform(90);
+            fg.DrawString("Central Rams Arcade", drawFont, drawBrush, new Rectangle());
+            fg.ResetTransform();
+            ClickToPlay.BackColor = Color.Green;
+
+            Thread.Sleep(1000);
+            Controls.Clear();
+            BackColor = Color.Black;
+
+            Pen Borderline = new Pen(Color.Blue);
+            SolidBrush PacMan = new SolidBrush(Color.Yellow);
+
+            formGraphics.DrawLine(Borderline, 5, 20, 100, 100);
+            
+        }
+    }
+}
